d8e072ade2fb30518616d1c0548acda71e2f8a41
felipeportari <felipe.portari7@gmail.com>
2023-11-20 19:00:27 -0300

    first commit

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+}

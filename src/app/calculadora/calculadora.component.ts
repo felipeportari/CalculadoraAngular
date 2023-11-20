685f955c0d08a3c000048150d3686a5ba59c230f
felipeportari <felipe.portari7@gmail.com>
2023-11-20 19:25:41 -0300

    Adicionando calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+  result: number = 0;
 
+  onSubmit(f: NgForm) {
+    let numA: number = f.value.numA
+    let numB: number = f.value.numB
+    let operator: string = f.value.operator
+
+    this.result = eval(`${numA}${operator}${numB}`)
+  }
 }
